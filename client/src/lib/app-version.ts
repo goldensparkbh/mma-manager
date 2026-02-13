fc06628e8e5dc55daf215ec29fa3c4c552fd0644
sparkflow0 <sparkflow2030@gmail.com>
2026-02-13 15:22:33 +0300

    'new'

--- client/src/lib/app-version.ts
+++ client/src/lib/app-version.ts
@@ -1,6 +1,6 @@
 export const APP_VERSION = {
-    number: "1.0.9",
-    date: "2026-02-06",
+    number: "1.1.0",
+    date: "2026-02-13",
     name: "New Release",
     repo: "sparkflow0/clubmanager" // Default repository
 }
